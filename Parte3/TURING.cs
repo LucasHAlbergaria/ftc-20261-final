41f40ecdb9f12f4d7e93603bc8680a098c8b0513
LucasHAlbergaria <lucashenriquealbergaria01@gmail.com>
2026-06-04 11:03:38 -0300

    Implementação da classe Turing com transições e estados definidos

---
+++ Parte3/TURING.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Parte3
+{
+    public class Turing
+    {
+        Dictionary<int, char> fita;
+        Dictionary<(string, char), (string, char, char)> transicoes;
+        int cabecote;
+        string estadoAtual;
+
+        public MaquinaTuring()
+        {
+            fita = new Dictionary<int, char>();
+            transicoes = new Dictionary<(string, char), (string, char, char)>();
+            cabecote = 0;
+            estadoAtual = "q0";
+
+            // ========================== Q0 ============================
+            transicoes.Add(
+                ("q0", 'a'),
+                ("q1", 'X', 'D')
+            );
+
+            transicoes.Add(
+                ("q0", 'X'),
+                ("q0", 'X', 'D')
+            );
+
+            transicoes.Add(
+                ("q0", 'Y'),
+                ("q4", 'Y', 'D')
+            );
+
+            transicoes.Add(
+                ("q0", 'Z'),
+                ("q4", 'Z', 'D')
+            );
+
+            transicoes.Add(
+                ("q0", 'b'),
+                ("qrej", 'b', 'D')
+            );
+
+            transicoes.Add(
+                ("q0", 'c'),
+                ("qrej", 'c', 'D')
+            );
+
+            transicoes.Add(
+                ("q0", '_'),
+                ("qrej", '_', 'D')
+            );
+
+            // ========================== Q1 ============================
+            transicoes.Add(
+                ("q1", 'a'),
+                ("q1", 'a', 'D')
+            );
+
+            transicoes.Add(
+                ("q1", 'Y'),
+                ("q1", 'Y', 'D')
+            );
+
+            transicoes.Add(
+                ("q1", 'b'),
+                ("q2", 'Y', 'D')
+            );
+
+            transicoes.Add(
+                ("q1", 'c'),
+                ("qrej", 'c', 'D')
+            );
+
+            transicoes.Add(
+                ("q1", 'Z'),
+                ("qrej", 'Z', 'D')
+            );
+
+            transicoes.Add(
+                ("q1", '_'),
+                ("qrej", '_', 'D')
+            );
+
+            // ========================== Q2 ============================
+            transicoes.Add(
+                ("q2", 'b'),
+                ("q2", 'b', 'D')
+            );
+
+            transicoes.Add(
+                ("q2", 'Z'),
+                ("q2", 'Z', 'D')
+            );
+
+            transicoes.Add(
+                ("q2", 'c'),
+                ("q3", 'Z', 'E')
+            );
+
+            transicoes.Add(
+                ("q2", 'a'),
+                ("qrej", 'a', 'D')
+            );
+
+            transicoes.Add(
+                ("q2", 'Y'),
+                ("qrej", 'Y', 'D')
+            );
+
+            transicoes.Add(
+                ("q2", '_'),
+                ("qrej", '_', 'D')
+            );
+
+            // ========================== Q3 ============================
+            transicoes.Add(
+                ("q3", 'a'),
+                ("q3", 'a', 'E')
+            );
+
+            transicoes.Add(
+                ("q3", 'b'),
+                ("q3", 'b', 'E')
+            );
+
+            transicoes.Add(
+                ("q3", 'Y'),
+                ("q3", 'Y', 'E')
+            );
+
+            transicoes.Add(
+                ("q3", 'Z'),
+                ("q3", 'Z', 'E')
+            );
+
+            transicoes.Add(
+                ("q3", 'X'),
+                ("q0", 'X', 'D')
+            );
+
+            transicoes.Add(
+                ("q3", '_'),
+                ("qrej", '_', 'D')
+            );
+
+            // ========================== Q4 ============================
+            transicoes.Add(
+                ("q4", 'Y'),
+                ("q4", 'Y', 'D')
+            );
+
+            transicoes.Add(
+                ("q4", 'Z'),
+                ("q4", 'Z', 'D')
+            );
+
+            transicoes.Add(
+                ("q4", '_'),
+                ("qacc", '_', 'D')
+            );
+
+            transicoes.Add(
+                ("q4", 'a'),
+                ("qrej", 'a', 'D')
+            );
+
+            transicoes.Add(
+                ("q4", 'b'),
+                ("qrej", 'b', 'D')
+            );
+
+            transicoes.Add(
+                ("q4", 'c'),
+                ("qrej", 'c', 'D')
+            );
+
+            transicoes.Add(
+                ("q4", 'X'),
+                ("qrej", 'X', 'D')
+            );
+        }
+
+        public void Executar(string entrada)
+        {
+        }
+    }
+}
